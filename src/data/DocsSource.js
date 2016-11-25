842e2f49b1b3d51f48f061fe27ee3371fc56139e
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-11-24 21:29:57 -0500

    Overhaul sources, add cache fallback if ratelimited

---
+++ src/data/DocsSource.js
@@ -0,0 +1,51 @@
+import request from 'superagent';
+
+export default class DocsSource {
+  constructor(options) {
+    this.id = options.id;
+    this.name = options.name;
+    this.global = options.global;
+    this.repo = options.repo;
+    this.defaultTag = options.defaultTag || 'master';
+    this.defaultFile = options.defaultFile || { category: 'general', id: 'welcome' };
+    this.source = options.source || `https://github.com/${this.repo}/blob/`;
+    this.branchFilter = options.branchFilter || (branch => branch !== 'master');
+    this.tagFilter = options.tagFilter || (() => true);
+    this.tags = null;
+  }
+
+  fetchTags() {
+    if (this.tags) return Promise.resolve(this.tags);
+    return Promise.all([
+      request.get(`https://api.github.com/repos/${this.repo}/branches`),
+      request.get(`https://api.github.com/repos/${this.repo}/tags`),
+    ]).then(responses => [responses[0].body, responses[1].body], err => {
+      if (localStorage[`source-${this.id}`]) {
+        console.error(err);
+        const cache = JSON.parse(localStorage[`source-${this.id}`]);
+        return [cache.branches, cache.tags];
+      }
+      throw err;
+    }).then(data => {
+      const branches = data[0];
+      const tags = data[1];
+      this.tags = [this.defaultTag];
+      localStorage[`source-${this.id}`] = JSON.stringify({ branches, tags });
+
+      for (const branch of branches) {
+        if (branch.name !== this.defaultTag && this.branchFilter(branch.name)) this.tags.push(branch.name);
+      }
+      for (const tag of tags) {
+        if (tag.name !== this.defaultTag && this.tagFilter(tag.name)) this.tags.push(tag.name);
+      }
+
+      return this.tags;
+    });
+  }
+
+  fetchDocs(tag) {
+    return request.get(`https://raw.githubusercontent.com/${this.repo}/docs/${tag}.json`).then(res =>
+      JSON.parse(res.text)
+    );
+  }
+}
--- src/data/MainSource.js
+++ src/data/MainSource.js
@@ -1,42 +1,12 @@
-const request = require('superagent');
-const semver = require('semver');
+import semver from 'semver';
+import DocsSource from './DocsSource';
 
-export default {
+const branchBlacklist = new Set(['gh-pages', 'gh-pages-dev', 'docs', 'webpack', 'indev-old', 'v8']);
+export default new DocsSource({
   id: 'main',
   name: 'Main library',
-  defaultTag: 'master',
-  defaultFile: { category: 'general', id: 'welcome' },
   global: 'Discord',
-  source: 'https://github.com/hydrabolt/discord.js/blob/',
-  blacklist: new Set(['gh-pages', 'gh-pages-dev', 'docs', 'webpack', 'v8', 'indev-old']),
-  tags: null,
-
-  fetchTags() {
-    if (this.tags) return Promise.resolve(this.tags);
-    return Promise.all([
-      request.get('https://api.github.com/repos/hydrabolt/discord.js/branches'),
-      request.get('https://api.github.com/repos/hydrabolt/discord.js/tags'),
-    ]).then(responses => {
-      const branches = responses[0].body;
-      const tags = responses[1].body;
-      this.tags = ['master'];
-
-      for (const branch of branches) {
-        if (branch.name !== 'master' && !this.blacklist.has(branch.name)) this.tags.push(branch.name);
-      }
-
-      for (const tag of tags) {
-        const name = tag.name.replace(/^v/, '');
-        if (semver.gte(name, '9.0.0')) this.tags.push(name);
-      }
-
-      return this.tags;
-    });
-  },
-
-  fetchDocs(tag) {
-    return request.get(`https://raw.githubusercontent.com/hydrabolt/discord.js/docs/${tag}.json`).then(res =>
-      JSON.parse(res.text)
-    );
-  },
-};
+  repo: 'hydrabolt/discord.js',
+  branchFilter: branch => !branchBlacklist.has(branch),
+  tagFilter: tag => semver.gte(tag.replace(/^v/, ''), '9.0.0'),
+});
