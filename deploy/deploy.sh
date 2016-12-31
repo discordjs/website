f80b39b21bb9c6c8f758b72e561c3c5c401fbf92
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2016-12-30 20:55:16 -0500

    Add Travis builds

---
+++ deploy/deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Adapted from https://gist.github.com/domenic/ec8b0fc8ab45f39403dd.
+
+set -e
+
+if [ "$TRAVIS_BRANCH" != "stable" -o -n "$TRAVIS_TAG" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo -e "\e[36m\e[1mNot building for a stable branch push - building without deploying."
+  npm run build
+  exit 0
+fi
+
+echo -e "\e[36m\e[1mBuilding for a stable branch push - building and deploying."
+
+# Initialise some useful variables
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+# Decrypt and add the ssh key
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy/deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+# Checkout the repo in the target branch
+TARGET_BRANCH="gh-pages"
+git clone $REPO dist -b $TARGET_BRANCH
+
+# Build the site
+npm run build
+
+# Commit and push
+cd out
+git add .
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+git commit -m "Site build: ${SHA}" || true
+git push $SSH_REPO $TARGET_BRANCH
