d1c67f3aa950bf2e9f1668551bed9164a6893cbe
Gus Caplan <me@gus.host>
2017-04-26 17:10:51 -0500

    Update shortlinks.js

--- src/shortlinks.js
+++ src/shortlinks.js
@@ -2,6 +2,5 @@ module.exports = {
   npm: 'https://www.npmjs.com/package/discord.js',
   yarn: 'https://yarnpkg.com/en/package/discord.js',
   github: 'https://github.com/hydrabolt/discord.js',
-  snek: 'https://gus.host',
   donate: 'https://www.patreon.com/discordjs',
 };
