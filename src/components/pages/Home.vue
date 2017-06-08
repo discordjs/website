c28e1248e593cfdcb0615e081dce4506e1630c6c
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2017-06-07 22:13:46 -0400

    Switch to User#tag for the homepage example

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -26,7 +26,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.username}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
