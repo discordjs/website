d45ad2a5abd1b38d3938e30b8fe17b201a77b86f
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2017-11-14 00:25:30 -0500

    Completely overhaul search

--- src/components/docs/Search.vue
+++ src/components/docs/Search.vue
@@ -3,62 +3,25 @@
     <h1>Search</h1>
     <input v-model.trim="search" type="search" />
 
+    <div id="toggles">
+      <label><input type="checkbox" v-model="toggles['classes']" /> Classes</label>
+      <label><input type="checkbox" v-model="toggles['props']" /> Properties</label>
+      <label><input type="checkbox" v-model="toggles['methods']" /> Methods</label>
+      <label><input type="checkbox" v-model="toggles['events']" /> Events</label>
+      <label><input type="checkbox" v-model="toggles['typedefs']" /> Typedefs</label>
+    </div>
 
     <transition name="fade" mode="out-in">
-      <div v-if="search && search.length > 1">
-        <h2 v-if="search && search.length > 1">Results for "{{ search }}"</h2>
+      <div v-if="search && search.length > 2">
+        <h2 v-if="search && search.length > 2">Results for "{{ search }}"</h2>
 
         <transition name="fade" mode="out-in">
-          <transition-group name="animated-list" tag="ul" v-if="results.count > 0" key="results">
-            <li v-if="results.classes.length > 0" key="classes" class="animated-list-item">
-              Classes
-              <ul>
-                <li v-for="clarse in results.classes" :key="clarse">
-                  <router-link :to="{ name: 'docs-class', params: { class: clarse } }">{{ clarse }}</router-link>
-                </li>
-              </ul>
-            </li>
-
-            <li v-if="results.typedefs.length > 0" key="typedefs" class="animated-list-item">
-              Typedefs
-              <ul>
-                <li v-for="typedef in results.typedefs">
-                  <router-link :to="{ name: 'docs-typedef', params: { typedef } }">{{ typedef }}</router-link>
-                </li>
-              </ul>
-            </li>
-
-            <li v-if="results.methods.length > 0"  key="methods" class="animated-list-item">
-              Methods
-              <ul>
-                <li v-for="method in results.methods">
-                  <router-link :to="{ name: 'docs-class', params: { class: method.class }, query: { scrollTo: scopedScrollTo(method) } }">
-                    {{ method.class }}{{ method.scope === 'static' ? '.' : '#' }}{{ method.name }}
-                  </router-link>
-                </li>
-              </ul>
-            </li>
-
-            <li v-if="results.props.length > 0" key="properties" class="animated-list-item">
-              Properties
-              <ul>
-                <li v-for="prop in results.props">
-                  <router-link :to="{ name: 'docs-class', params: { class: prop.class }, query: { scrollTo: scopedScrollTo(prop) } }">
-                    {{ prop.class }}{{ prop.scope === 'static' ? '.' : '#' }}{{ prop.name }}
-                  </router-link>
-                </li>
-              </ul>
-            </li>
-
-            <li v-if="results.events.length > 0" key="events" class="animated-list-item">
-              Events
-              <ul>
-                <li v-for="event in results.events">
-                  <router-link :to="{ name: 'docs-class', params: { class: event.class }, query: { scrollTo: event.name } }">
-                    {{ event.class }}#{{ event.name }}
-                  </router-link>
-                </li>
-              </ul>
+          <transition-group name="animated-list" tag="ul" v-if="results.length > 0" key="results">
+            <li v-for="result in results" :key="`${result.key || result.name}`" class="animated-list-item">
+              <router-link :to="result.route">
+                <span class="badge">{{ result.badge }}</span>
+                {{ result.name }}
+              </router-link>
             </li>
           </transition-group>
 
@@ -66,12 +29,15 @@
         </transition>
       </div>
 
-      <p v-else key="short">Your search query must be at least two characters.</p>
+      <p v-else key="short">Your search query must be at least three characters.</p>
     </transition>
   </div>
 </template>
 
 <script>
+  import levenshtein from 'js-levenshtein';
+  import { sort } from 'timsort';
+
   export default {
     name: 'docs-search',
     props: ['docs', 'showPrivate'],
@@ -79,84 +45,69 @@
     data() {
       return {
         search: this.$route.query.q,
+        toggles: { classes: true, props: true, methods: true, events: true, typedefs: true },
       };
     },
 
     computed: {
       results() {
         const q = this.search.toLowerCase();
-        const results = {
-          classes: [],
-          typedefs: [],
-          methods: [],
-          props: [],
-          events: [],
-          count: 0,
-        };
+        const results = [];
 
-        this.docs.classes.forEach(c => {
-          if (!this.showPrivate && c.access === 'private') return;
+        for (const clarse of this.docs.classes) {
+          if (!this.showPrivate && clarse.access === 'private') continue;
 
-          if (c.name.toLowerCase().includes(q)) {
-            results.classes.push(c.name);
-            results.count++;
+          if (this.toggles.classes) {
+            const clarseScore = score(q, clarse.name, 1);
+            if (clarseScore >= scoreThreshold) {
+              results.push({
+                score: clarseScore + 0.1,
+                name: clarse.name,
+                route: { name: 'docs-class', params: { class: clarse.name } },
+                badge: 'C',
+              });
+            }
           }
 
-          if (c.methods) {
-            c.methods.forEach(m => {
-              if (!this.showPrivate && m.access === 'private') return;
-              if (m.name.toLowerCase().includes(q)) {
-                results.methods.push({
-                  name: m.name,
-                  scope: m.scope,
-                  class: c.name,
-                });
-                results.count++;
-              }
+          for (const group of ['props', 'methods', 'events']) {
+            if (!clarse[group] || !this.toggles[group]) continue;
+            for (const item of clarse[group]) {
+              if (!this.showPrivate && item.access === 'private') continue;
+              const theScore = score(q, item.name);
+              if (theScore < scoreThreshold) continue;
+              const name = fullName(item, clarse, group);
+              results.push({
+                score: theScore,
+                name,
+                route: { name: 'docs-class', params: { class: clarse.name }, query: { scrollTo: this.scopedName(item) } },
+                badge: group[0].toUpperCase(),
+                key: group === 'events' ? `e-${name}` : null,
+              });
+            }
+          }
+        }
+
+        if (this.toggles.typedefs) {
+          for (const typedef of this.docs.typedefs) {
+            if (!this.showPrivate && typedef.access === 'private') continue;
+            const typedefScore = score(q, typedef.name, 1);
+            if (typedefScore < scoreThreshold) continue;
+            results.push({
+              score: typedefScore + 0.1,
+              name: typedef.name,
+              route: { name: 'docs-typedef', params: { typedef: typedef.name } },
+              badge: 'T',
             });
           }
+        }
 
-          if (c.props) {
-            c.props.forEach(p => {
-              if (!this.showPrivate && p.access === 'private') return;
-              if (p.name.toLowerCase().includes(q)) {
-                results.props.push({
-                  name: p.name,
-                  scope: p.scope,
-                  class: c.name,
-                });
-                results.count++;
-              }
-            });
-          }
-
-          if (c.events) {
-            c.events.forEach(e => {
-              if (!this.showPrivate && e.access === 'private') return;
-              if (e.name.toLowerCase().includes(q)) {
-                results.events.push({
-                  name: e.name,
-                  class: c.name,
-                });
-                results.count++;
-              }
-            });
-          }
-        });
-
-        this.docs.typedefs.forEach(t => {
-          if (t.name.toLowerCase().includes(q)) {
-            results.typedefs.push(t.name);
-            results.count++;
-          }
-        });
-
+        sort(results, (a, b) => b.score - a.score);
         return results;
       },
     },
 
     methods: {
-      scopedScrollTo(item) {
+      scopedName(item) {
         return `${item.scope === 'static' ? 's-' : ''}${item.name}`;
       },
     },
@@ -173,15 +124,34 @@
       },
     },
   };
+
+  const scoreThreshold = 0.5;
+
+  function score(s1, s2, identicalWeight) {
+    let longer = s1, shorter = s2.toLowerCase();
+    if (longer === shorter) return 1 + (identicalWeight === undefined ? 0.5 : identicalWeight);
+    if (s1.length < s2.length) {
+      longer = s2;
+      shorter = s1;
+    }
+    const longerLength = longer.length;
+    if (longerLength === 0) return 1;
+    return (longerLength - levenshtein(longer, shorter)) / longerLength;
+  }
+
+  function fullName(child, parent, group) {
+    return `${parent.name + (child.scope === 'static' ? '.' : '#')}${child.name}${group === 'methods' ? '()' : ''}`;
+  }
 </script>
 
 <style lang="scss">
+  @import '../../styles/theming';
   @import '../../styles/mq';
 
   #docs-search {
     padding: 16px 32px;
 
-    input {
+    input[type="search"] {
       margin: 4px 2px;
       width: 16rem;
       max-width: 100%;
@@ -191,8 +161,49 @@
       }
     }
 
+    #toggles {
+      display: flex;
+      flex-direction: row;
+      align-items: stretch;
+      margin-top: 4px;
+
+      label {
+        flex: 1;
+        margin: 4px;
+      }
+
+      @include mq($until: desktop) {
+        flex-direction: column;
+      }
+    }
+
     ul {
-      margin-bottom: 24px;
+      padding-left: 24px;
+      list-style: none;
+    }
+
+    li {
+      margin-bottom: 4px;
+    }
+
+    .badge {
+      display: inline-block;
+      width: 0.8rem;
+      margin-left: 0;
+      margin-right: 8px;
+      padding: 3px 4px;
+      text-align: center;
+      font-size: 0.9rem;
+      transition: background-color 0.3s;
+    }
+
+    a {
+      display: inline-block;
+      height: 100%;
+
+      &:hover .badge {
+        background: lighten($color-primary, 20%);
+      }
     }
   }
 </style>
