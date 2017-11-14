c9378ca72f2ce1f76db34e4b1cb7c9571119bb82
Schuyler Cebulskie <Gawdl3y@gmail.com>
2017-11-14 11:31:33 -0500

    Hide RPC docs branch

--- src/data/RPCSource.js
+++ src/data/RPCSource.js
@@ -7,6 +7,6 @@ export default new DocsSource({
   global: 'RPC',
   repo: 'devsnek/discord-rpc',
   defaultTag: 'master',
-  branchFilter: branch => !branch.includes('greenkeeper'),
+  branchFilter: branch => branch !== 'docs' && !branch.includes('greenkeeper'),
   tagFilter: tag => semver.gte(tag.replace(/^v/, ''), '3.0.0'),
 });
