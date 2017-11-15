96052bf0b576ecba74d30106523837c01d4020ee
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2017-11-14 21:05:22 -0500

    Clean up router a bit

--- src/router.js
+++ src/router.js
@@ -1,27 +1,27 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HomePageComponent from './components/pages/Home.vue';
-import DocumentationPageComponent from './components/pages/Documentation.vue';
-import UnknownRoutePageComponent from './components/pages/UnknownRoute.vue';
-import DocsLoaderComponent from './components/docs/Loader.vue';
-import DocsViewerComponent from './components/docs/Viewer.vue';
-import FileViewerComponent from './components/docs/FileViewer.vue';
-import ClassViewerComponent from './components/docs/class-viewer/ClassViewer.vue';
-import TypedefViewerComponent from './components/docs/TypedefViewer.vue';
-import DocsSearchComponent from './components/docs/Search.vue';
+import HomePage from './components/pages/Home.vue';
+import DocumentationPage from './components/pages/Documentation.vue';
+import UnknownRoutePage from './components/pages/UnknownRoute.vue';
+import DocsLoader from './components/docs/Loader.vue';
+import DocsViewer from './components/docs/Viewer.vue';
+import FileViewer from './components/docs/FileViewer.vue';
+import ClassViewer from './components/docs/class-viewer/ClassViewer.vue';
+import TypedefViewer from './components/docs/TypedefViewer.vue';
+import DocsSearch from './components/docs/Search.vue';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    { path: '/', name: 'home', component: HomePageComponent },
-    { path: '/docs', name: 'docs', component: DocumentationPageComponent, children: [
-      { path: ':source', name: 'docs-source', component: DocsLoaderComponent, children: [
-        { path: ':tag', name: 'docs-tag', component: DocsViewerComponent, children: [
-          { path: 'search', name: 'docs-search', component: DocsSearchComponent },
-          { path: 'class/:class', name: 'docs-class', component: ClassViewerComponent },
-          { path: 'typedef/:typedef', name: 'docs-typedef', component: TypedefViewerComponent },
-          { path: ':category/:file', name: 'docs-file', component: FileViewerComponent },
+    { path: '/', name: 'home', component: HomePage },
+    { path: '/docs', name: 'docs', component: DocumentationPage, children: [
+      { path: ':source', name: 'docs-source', component: DocsLoader, children: [
+        { path: ':tag', name: 'docs-tag', component: DocsViewer, children: [
+          { path: 'search', name: 'docs-search', component: DocsSearch },
+          { path: 'class/:class', name: 'docs-class', component: ClassViewer },
+          { path: 'typedef/:typedef', name: 'docs-typedef', component: TypedefViewer },
+          { path: ':category/:file', name: 'docs-file', component: FileViewer },
         ] },
       ] },
     ] },
@@ -62,6 +62,6 @@ export default new Router({
     ] },
 
     // Catch-all
-    { path: '*', component: UnknownRoutePageComponent },
+    { path: '*', component: UnknownRoutePage },
   ],
 });
