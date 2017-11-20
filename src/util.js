f03156de46a1954e530e7e58af0234588bfe2f4c
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2017-11-19 19:17:53 -0500

    Fix JSDoc link tags with builtins

--- src/util.js
+++ src/util.js
@@ -34,11 +34,11 @@ export function parseLink(link, docs) {
   if (docs.links[split[0]]) {
     return {
       text: text || link,
-      link: {
+      link: typeof docs.links[split[0]] === 'object' ? {
         name: docs.links[split[0]].name,
         params: docs.links[split[0]].params,
         query: { scrollTo: split[1] ? `${split[1] === '.' ? 's-' : ''}${split[2]}` : undefined },
-      },
+      } : docs.links[split[0]],
     };
   }
 
