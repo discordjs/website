e89aa4c95c3c0586fb9301ec8c1aa2d542059cb3
Sanctuary <sanc.pw@gmail.com>
2018-04-30 17:04:00 -0300

    Fix CSS scoping issue with badges
    Somehow missed this in my last PR but the `.badge` and `.score`
    selectors in the SearchResults component should've gone under the
    `.results-list li` selector. Without doing so, it affected other badges
    around the site.

--- src/components/docs/SearchResults.vue
+++ src/components/docs/SearchResults.vue
@@ -37,30 +37,30 @@
 
     li {
       margin-bottom: 4px;
+
+      .badge {
+        display: inline-block;
+        width: 0.8rem;
+        margin-left: 0;
+        margin-right: 8px;
+        padding: 3px 4px;
+        text-align: center;
+        font-size: 0.9rem;
+        opacity: 1;
+        transition: background-color 0.3s;
+      }
+
+      .score {
+        display: inline-block;
+        position: relative;
+        right: 1.7rem;
+        width: 0;
+        margin: 0;
+        font-size: 0.7rem;
+        overflow: visible;
+        color: lighten($color-content-text, 40%);
+      }
     }
-  }
-
-  .badge {
-    display: inline-block;
-    width: 0.8rem;
-    margin-left: 0;
-    margin-right: 8px;
-    padding: 3px 4px;
-    text-align: center;
-    font-size: 0.9rem;
-    opacity: 1;
-    transition: background-color 0.3s;
-  }
-
-  .score {
-    display: inline-block;
-    position: relative;
-    right: 1.7rem;
-    width: 0;
-    margin: 0;
-    font-size: 0.7rem;
-    overflow: visible;
-    color: lighten($color-content-text, 40%);
   }
 
   #app.dark .results-list .score {
