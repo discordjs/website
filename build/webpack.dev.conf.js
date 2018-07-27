270074a2ef46ab42196c88c05a4544910aeaedda
Will Nelson <nelson.will@live.com>
2018-07-26 17:12:20 -0700

    make csp actually correct

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -14,7 +14,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   },
   // cheap-module-eval-source-map is faster for development
   devtool: config.dev.devtool,
-  
+
   // these devServer options should be customized in /config/index.js
   devServer: {
     historyApiFallback: true,
@@ -36,13 +36,13 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': require('../config/dev.env')
-    }), 
+    }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
     new webpack.NoEmitOnErrorsPlugin(),
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename: 'index.html',
+      filename: 'index.dev.html',
       template: 'index.html',
       inject: true
     }),
