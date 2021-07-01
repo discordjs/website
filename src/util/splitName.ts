44b392a2c5ec507bf421839801795c6a78db6902
Drahcirius <hardy263@gmail.com>
2021-07-01 16:51:10 -0400

    fix(search): properly index constants and make their search results better

--- src/util/splitName.ts
+++ src/util/splitName.ts
@@ -1,7 +1,11 @@
-// Split names into 3 types of words, using userGroupDMChannel as fictional example
+// For camelcase and pascalcase, split names into 3 types of words, using userGroupDMChannel as fictional example
 // Acronyms, e.g. DM
 // Capital, e.g. Group, Channel
 // Lowercase, e.g. user
+// If the name is in screaming snake case, split by underscore
 export function splitName(name: string) {
+	if (/^[_A-Z]+$/.test(name)) {
+		return name.match(/[A-Z]+/g) ?? [];
+	}
 	return name.match(/(([A-Z]{2,})(?=[A-Z]))|[A-Z][a-z]+|[a-z]+/g) ?? [];
 }
