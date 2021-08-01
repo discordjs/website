06caaa7c6962e606f9d6fc55602730d962fc0c80
Megatank58 <tanumegatank@gmail.com>
2021-08-01 16:35:04 +0530

    fix: /docs now go to the actual documentation
    All URL no longer have a #/ at beginning or #/ at end so /docs go to /docs/main/stable/general/welcome and other links such as /null go to 404 page as they maybe should?

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import routes from 'virtual:generated-pages';
 
 export default createRouter({
-	history: createWebHashHistory(),
+	history: createWebHistory(),
 	routes,
 });
