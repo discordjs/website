e44c4605798eeff71f94d0e4da659f0b291a2385
Sugden <28943913+NotSugden@users.noreply.github.com>
2021-08-08 18:33:37 +0100

    chore: update commando branch

--- src/data/CommandoSource.ts
+++ src/data/CommandoSource.ts
@@ -8,6 +8,7 @@ export default new DocsSource({
 	name: 'Commando',
 	global: 'Commando',
 	repo: 'discordjs/Commando',
+	defaultTag: 'master',
 	branchFilter: (branch: string) => !branchBlacklist.has(branch) && !branch.startsWith('dependabot/'),
 	tagFilter: (tag: string) => semver.gt(tag.replace(/^v/, ''), '0.4.1'),
 });
