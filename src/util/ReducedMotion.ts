13b1a47ff7dd3ef76e75e8f064ce08163847e721
Jiralite <33201955+Jiralite@users.noreply.github.com>
2021-10-19 22:46:24 +0100

    feat: sync with OS

--- src/util/ReducedMotion.ts
+++ src/util/ReducedMotion.ts
@@ -1,10 +1,11 @@
+import { watchEffect } from 'vue';
 import { useMediaQuery, useStorage } from '@vueuse/core';
 
 const keyName = 'reduced-motion';
 const prefersReducedMotion = useMediaQuery('(prefers-reduced-motion)');
 const state = useStorage(keyName, prefersReducedMotion.value);
 
-export const usePreferredReducedMotion = prefersReducedMotion.value ? prefersReducedMotion : state;
+export const usePreferredReducedMotion = state;
 
 if (usePreferredReducedMotion.value) {
 	document.documentElement.classList.add('reduce-motion');
@@ -14,8 +15,17 @@ if (usePreferredReducedMotion.value) {
 	document.documentElement.classList.remove('reduce-motion');
 }
 
+let firstLoad = true;
+
 export const toggleReducedMotion = (value: boolean): void => {
+	if (firstLoad) {
+		firstLoad = false;
+		return;
+	}
+
 	state.value = value;
 	document.documentElement.classList.toggle('full-motion');
 	document.documentElement.classList.toggle('reduce-motion');
 };
+
+watchEffect(() => toggleReducedMotion(prefersReducedMotion.value));
