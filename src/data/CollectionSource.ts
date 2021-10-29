1cfada9a2abd979bf2fddfc38d543c36b73ac59b
iCrawl <buechler.noel@outlook.com>
2021-10-29 05:52:24 +0200

    chore: update sources

--- src/data/CollectionSource.ts
+++ src/data/CollectionSource.ts
@@ -1,3 +1,5 @@
+import semver from 'semver';
+
 import DocsSource from './DocsSource';
 
 const branchBlacklist = new Set(['docs']);
@@ -6,6 +8,7 @@ export default new DocsSource({
 	name: 'Collection',
 	global: 'Collection',
 	repo: 'discordjs/collection',
-	defaultTag: 'main',
+	defaultTag: 'stable',
 	branchFilter: (branch: string) => !branchBlacklist.has(branch) && !branch.startsWith('dependabot/'),
+	tagFilter: (tag: string) => semver.gte(tag.replace(/^v/, ''), '0.3.0'),
 });
