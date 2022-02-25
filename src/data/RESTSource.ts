2f4d51bec60783f23cf87e21ef84d38a7e46cb74
Binit Ghimire <thebinitghimire@gmail.com>
2022-02-25 10:39:13 +0545

    fix(RESTSource): fix defaultTag for @discordjs/rest

--- src/data/RESTSource.ts
+++ src/data/RESTSource.ts
@@ -9,7 +9,7 @@ export default new DocsSource({
 	global: 'REST',
 	docsRepo: 'discordjs/docs',
 	repo: 'discordjs/discord.js',
-	defaultTag: 'stable',
+	defaultTag: 'main',
 	branchFilter: (branch: string) => !branchBlacklist.has(branch) && !branch.startsWith('dependabot/'),
 	tagFilter: (tag: string) =>
 		semver.gt(tag.replace(/(^@\w+\/\w+@v?)?(?<semver>\d+.\d+.\d+)-?.*/, '$<semver>'), '0.2.0'),
