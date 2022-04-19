fb14633761e713fa6341b14e56cf4b3ba7f3d897
iCrawl <buechler.noel@outlook.com>
2022-04-19 13:40:00 +0200

    fix: update virtual pages

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
-import routes from 'virtual:generated-pages';
 import { createRouter, createWebHashHistory } from 'vue-router';
+import routes from '~pages';
 
 export default createRouter({
 	history: createWebHashHistory(),
