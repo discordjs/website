bcd67918fb9e37ad5d9b57b223c2b0fba43f6562
iCrawl <buechler.noel@outlook.com>
2022-04-19 15:42:43 +0200

    feat: change max-width to max-w-prose on the frontpage

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,7 +7,7 @@
 	</div>
 
 	<div
-		class="prose prose-discord dark:prose-light lg:prose-lg px-6 mx-auto pb-8 w-full xl:grid xl:grid-cols-2 xl:gap-x-12 xl:max-w-7xl"
+		class="prose prose-discord dark:prose-light lg:prose-lg px-6 mx-auto pb-8 w-full xl:grid xl:grid-cols-2 xl:gap-x-12 xl:max-w-prose"
 	>
 		<div class="col-span-full">
 			<h2>About</h2>
