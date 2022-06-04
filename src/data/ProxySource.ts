25181c6315531b44b8ceb064e37d4173dbee8b8c
didinele <didinele.dev@gmail.com>
2022-06-04 14:37:41 +0300

    fix(ProxySource): remove semver checks

--- src/data/ProxySource.ts
+++ src/data/ProxySource.ts
@@ -1,17 +1,11 @@
-import semver from 'semver';
-
 import DocsSource from './DocsSource';
 
 const branchBlacklist = new Set(['docs', 'v8', 'v11', 'v12', 'v13']);
 export default new DocsSource({
 	id: 'proxy',
-	name: 'proxy',
-	global: 'proxy',
+	name: 'Proxy',
+	global: 'Proxy',
 	docsRepo: 'discordjs/docs',
 	repo: 'discordjs/discord.js',
-	defaultTag: 'stable',
 	branchFilter: (branch: string) => !branchBlacklist.has(branch) && !branch.startsWith('dependabot/'),
-	tagFilter: (tag: string) =>
-		tag.includes('@discordjs/proxy') &&
-		semver.gt(tag.replace(/(^@.*\/.*@v?)?(?<semver>\d+.\d+.\d+)-?.*/, '$<semver>'), '0.2.0'),
 });
