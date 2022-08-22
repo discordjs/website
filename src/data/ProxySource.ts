7c931b430ccf08d48ae6b5cd43dc52f815601f23
Jiralite <33201955+Jiralite@users.noreply.github.com>
2022-08-22 12:09:25 +0100

    fix: Filter tags for proxy and ws (#140)

--- src/data/ProxySource.ts
+++ src/data/ProxySource.ts
@@ -1,3 +1,4 @@
+import semver from 'semver';
 import DocsSource from './DocsSource';
 
 const branchBlacklist = new Set(['stable', 'docs', 'v8', 'v11', 'v12', 'v13']);
@@ -8,4 +9,7 @@ export default new DocsSource({
 	docsRepo: 'discordjs/docs',
 	repo: 'discordjs/discord.js',
 	branchFilter: (branch: string) => !branchBlacklist.has(branch) && !branch.startsWith('dependabot/'),
+	tagFilter: (tag) =>
+		tag.includes('@discordjs/proxy') &&
+		semver.gte(tag.replace(/(^@.*\/.*@v?)?(?<semver>\d+.\d+.\d+)-?.*/, '$<semver>'), '1.0.1'),
 });
