d6e3a5fcd39ceb41b256216d5bcc20a309781b54
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-04-20 19:02:43 +0100

    fix: Filter branches explicitly

--- src/data/MainSource.ts
+++ src/data/MainSource.ts
@@ -1,14 +1,12 @@
 import semver from 'semver';
-
 import DocsSource from './DocsSource';
 
-const branchBlacklist = new Set(['docs', 'webpack', 'v8']);
 export default new DocsSource({
 	id: 'discord.js',
 	name: 'discord.js',
 	global: 'discord.js',
 	docsRepo: 'discordjs/docs',
 	repo: 'discordjs/discord.js',
-	branchFilter: (branch: string) => !branchBlacklist.has(branch) && !branch.startsWith('dependabot/'),
+	branchFilter: (branch) => branch === "main" || /^v1[3-9]$/.test(branch),
 	tagFilter: (tag: string) => semver.gte(tag.replace(/(^@.*\/.*@v?)?(?<semver>\d+.\d+.\d+)-?.*/, '$<semver>'), '9.0.0'),
 });
